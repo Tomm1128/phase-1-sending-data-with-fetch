5c258d957c25c179a880225a82f82d09d123b00f
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-15 14:01:29 -0400

    Create submitData to add user name and email to db

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+
+const submitData = (userName, userEmail) => {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    }, 
+    body: JSON.stringify({name: userName, email: userEmail}),
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .then((userObj) => {
+    
+  })
+}
