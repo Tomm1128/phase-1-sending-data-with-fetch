2bbd0ae0ce197bd211a084d5cfaa208f326d8246
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-15 14:14:06 -0400

    Catch error on user submission

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Add your code here
-
 const submitData = (userName, userEmail) => {
   return fetch("http://localhost:3000/users", {
     method: "POST",
@@ -16,6 +14,14 @@ const submitData = (userName, userEmail) => {
     const body = document.querySelector("body")
     const p = document.createElement("p")
     p.textContent = userObj.id
+    body.appendChild(p)
+  })
+  .catch((error) => {
+    alert("Something went wrong, please refresh and try again")
+    const body = document.querySelector("body")
+    const errorMessage = error.message
+    const p = document.createElement("p")
+    p.textContent = errorMessage
     body.appendChild(p)
   })
 }
